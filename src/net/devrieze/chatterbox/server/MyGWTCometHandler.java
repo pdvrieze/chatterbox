e7e0e114db6dd06dbeb7ad76d1df54ff7110cc9b
Paul de Vrieze <paul@devrieze.net>
2012-07-20 22:09:58 +0100

    Clean up the Comet handler, and override doComet to set timeout and broadcaster

--- src/net/devrieze/chatterbox/server/MyGWTCometHandler.java
+++ src/net/devrieze/chatterbox/server/MyGWTCometHandler.java
@@ -2,27 +2,35 @@ package net.devrieze.chatterbox.server;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.gwt.server.AtmosphereGwtHandler;
+import org.atmosphere.gwt.server.GwtAtmosphereResource;
 
 
 public class MyGWTCometHandler extends AtmosphereGwtHandler {
 
-  private static final long MAX_TIMEOUT = 300000l;
+  @Override
+  public int doComet(GwtAtmosphereResource pResource) throws ServletException, IOException {
+    Broadcaster broadcaster = BroadcasterFactory.getDefault().lookup(BROADCASTERNAME, true);
+    pResource.getAtmosphereResource().setBroadcaster(broadcaster);
+    return TIMEOUT;
+  }
+
+  private static final int TIMEOUT = -1;
   static final String BROADCASTERNAME = GWT_BROADCASTER_ID;
 
   @Override
   public void onRequest(AtmosphereResource pResource) throws IOException {
-    Broadcaster broadcaster = BroadcasterFactory.getDefault().lookup(BROADCASTERNAME, true);
-    pResource.setBroadcaster(broadcaster);
     try {
       super.onRequest(pResource);
     } catch (IllegalStateException e) {
       // Transport could not be determined. Just handle like regular request
       if ("GET".equals(pResource.getRequest().getMethod())) {
-        pResource.suspend(MAX_TIMEOUT, false);
+        pResource.suspend(TIMEOUT, false);
       }
     }
   }
